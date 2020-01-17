f9c3cad1846a54831ce3b21ac86debded25383b9
ChristopherAndres <christopher@chrisandres.net>
2020-01-17 15:45:32 -0600

    Update Robot.py

--- Robot.py
+++ Robot.py
@@ -1,21 +1,14 @@
 #Video Fixing Robot
 #By Chris Andres
 #Requires Ubuntu, Python3, ffmpeg and MediaInfo
-#
-#Importing required libarys
+
+#Importing required libs
 import os
 import argparse
 import subprocess
 import sys
 from datetime import datetime
 import shutil
-
-#Setting Timestamps
-
-#now = datetime.now()
-#timestamp2 = datetime.timestamp(now)
-# = str(timestamp2)
-#print("timestamp = ", timestamp)
 
 #Passing Arguments from the command line
 parser = argparse.ArgumentParser(description='A Tortual of Argparse')
@@ -29,8 +22,6 @@ outputFile = args.o
 
 #Starting Folder
 startingFolder = os.getcwd()
-print(startingFolder)
-
 print("Working on: " +inputFile)
 
 #Creating A working folder
@@ -48,31 +39,16 @@ heightCommand ="mediainfo --inform=" + "'" + "Video" +";" + "%Height%" + "'" + "
 fpsVarCmd2 = "mediainfo --inform=" + "'" + "Video" + ";" + "%FrameRate%" + "'" + " " + inputFile
 fpsVarCmd = str(fpsVarCmd2)
 
-#Running the MediaInfo commands
-#width = os.popen(widthComand).read()
-#height = os.popen(heightCommand).read()
-#fpsVar = os.popen(fpsVarCmd).read()
-
-#Printing the varibles, this is more for testing
-#print("The Width is: " + width)
-#print("The Height is: " + height)
-#print("The Frame Rate is: " + fpsVar)
-
-
 #Script used to rip frames from the video, and re render it
 ffmpegInput = "ffmpeg -i " + str(inputFile) + " -r 1/1 " + "$filename%03d.tiff"
 
 #running the ffmpeg script
 os.system(ffmpegInput)
 
-#this is more for testing
-#print(ffmpegInput)
-
 print("done")
 
 #ffmpeg rendering script
 ffmpegOutput = "ffmpeg -r 4 -i %03d.tiff -c:v libx264 -vf fps=30 -pix_fmt yuv420p " + outputFile
-#print(ffmpegOutput)
 
 #running the output script
 os.system(ffmpegOutput)
